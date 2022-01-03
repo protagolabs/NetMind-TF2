7948b050468187ca4c1baad531d48033ba1cd47a
Xing Di (底兴) <9487284+DetionDX@users.noreply.github.com>
2022-01-03 11:29:18 -0500

    Add files via upload

---
+++ pretrain/Bert/glue_eval/run_glue_sst.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# batch sizes: 8, 16, 32, 64, 128
+# learning rates: 3e-4, 1e-4, 5e-5, 3e-5
+
+for bs in 128 64 32 16 8; do for lr in 3e-4 1e-4 5e-5 3e-5
+    do
+    CUDA_VISIBLE_DEVICES=1 python run_glue.py \
+    --model_name_or_path="bert_saved_model_ep6/" \
+    --tokenizer_name="bert-base-uncased" \
+    --task_name="sst2" \
+    --per_device_train_batch_size=$bs \
+    --learning_rate=$lr \
+    --num_train_epochs=4 \
+    --do_train \
+    --do_eval \
+    --max_seq_length=512 \
+    --overwrite_output_dir \
+    --output_dir="./sst2_saved_model"
+    done; done
