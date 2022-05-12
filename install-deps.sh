77355a653eb47ef0a8c6dddf315940ad45c3be30
DetionDX <detiondx@gmail.com>
2022-05-12 08:41:57 -0400

    add install dev

--- install-deps.sh
+++ install-deps.sh
@@ -19,11 +19,11 @@ conda activate netmind-tf2
 # install the cuda and cudnn
 conda install -c anaconda cudnn
 
-# add cudnn to path, otherwise you will meet issue when running tensorflow as:
+# add cuda/cudnn to path, otherwise you will meet issue when running tensorflow as:
 # Could not load dynamic library 'libcudart.so.11.0'; dlerror: libcudart.so.11.0: cannot open shared object file: No such file or directory
 nano ~/.bashrc
 
-# add the following to the end
+# add the following cuda/cudnn path to the end
 
 export PATH=/home/xing/miniconda3/envs/netmind-tf2/lib:${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/home/xing/miniconda3/envs/netmind-tf2/lib:${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
