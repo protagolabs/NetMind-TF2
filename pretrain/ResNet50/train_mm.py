9c82a54e81f25067acf5200fdac58332f5ea9176
Xing Di (底兴) <detiondx@gmail.com>
2022-05-11 22:48:15 -0400

    Update train_mm.py

--- pretrain/ResNet50/train_mm.py
+++ pretrain/ResNet50/train_mm.py
@@ -24,7 +24,7 @@ class CosineDecayWithWarmUP(tf.keras.experimental.CosineDecay):
     @tf.function
     def __call__(self, step):
         if step <= self.warm_up_step:
-            return step / self.warm_up_step * self.initial_learning_rate
+            return tf.cast(step / self.warm_up_step * self.initial_learning_rate, tf.float32)
         else:
             return super(CosineDecayWithWarmUP, self).__call__(step - self.warm_up_step)
 
@@ -44,9 +44,11 @@ def train_step(dist_inputs):
 
         with tf.GradientTape() as tape:
             prediction = model(images)
-            ce = cross_entropy_batch(labels, prediction, label_smoothing=c.label_smoothing)
+            # ce = cross_entropy_batch(labels, prediction, label_smoothing=c.label_smoothing)
+            ce = tf.keras.losses.categorical_crossentropy(labels, prediction, label_smoothing=c.label_smoothing)
             l2 = l2_loss(model)
-            loss = ce + l2
+            # loss = ce + l2
+            loss = ce
 
             grads = tape.gradient(loss, model.trainable_variables)
             optimizer.apply_gradients(list(zip(grads, model.trainable_variables)))
@@ -78,6 +80,11 @@ def test_step(dist_inputs):
     acc = mirrored_strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_pred, axis=None)
     return mean_loss, acc
 
+def set_input_shape(img, label):
+    img = img.set_shape(c.input_shape)
+    label = label.set_shape([c.category_num])
+    return img, label
+
 ## Using `tf.distribute.Strategy` with custom training loops
 
 if __name__ == '__main__':
@@ -100,21 +107,25 @@ if __name__ == '__main__':
         model = ResNet(50)
 
         model.build(input_shape=(None,) + c.input_shape)
+        
         model.summary()
-        print('initial l2 loss:{:.4f}'.format(l2_loss(model)))
+
+        inputs = tf.keras.Input(shape=c.input_shape)
+
 
         # load pretrain
         if c.load_weight_file is not None:
             model.load_weights(c.load_weight_file)
-            print('pretrain weight l2 loss:{:.4f}'.format(l2_loss(model)))
+            # print('pretrain weight l2 loss:{:.4f}'.format(l2_loss(model)))
 
         # here we automatically change the iterations per epoch based on number of gpus
-        learning_rate_schedules = CosineDecayWithWarmUP(initial_learning_rate=c.initial_learning_rate * 2,
+        learning_rate_schedules = CosineDecayWithWarmUP(initial_learning_rate=c.initial_learning_rate * num_gpus,
                                                         decay_steps=c.epoch_num * int(c.iterations_per_epoch / num_gpus)  - int(c.warm_iterations / num_gpus),
-                                                        alpha=c.minimum_learning_rate,
+                                                        alpha=c.minimum_learning_rate * num_gpus,
                                                         warm_up_step=int(c.warm_iterations / num_gpus))
 
         optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate_schedules, momentum=0.9)
+
 
 
     
@@ -126,64 +137,53 @@ if __name__ == '__main__':
     # for inputs in train_data_iterator:
     #     print(train_step(inputs))
     #     # train_step(inputs)
+    # train_iterator = train_iterator.map(set_input_shape)
     dataset_train = train_iterator().batch(global_batch_size)
-    train_data_iterator = mirrored_strategy.experimental_distribute_dataset(dataset_train)
+    train_data_iterator = iter(mirrored_strategy.experimental_distribute_dataset(dataset_train))
 
 
     #  eval
-    dataset_eval = test_iterator().batch(global_batch_size, drop_remainder=True)
-    test_data_iterator = mirrored_strategy.experimental_distribute_dataset(dataset_eval)
-    # with open(c.log_file, 'a') as f:
+    dataset_eval = test_iterator().batch(global_batch_size)
+    test_data_iterator = iter(mirrored_strategy.experimental_distribute_dataset(dataset_eval))
 
 
-    #     dataset_eval = test_iterator().batch(global_batch_size)
-    #     test_data_iterator = iter(mirrored_strategy.experimental_distribute_dataset(dataset_eval))
+    with open(c.log_file, 'a') as f:
 
-    #     sum_ce = 0
-    #     sum_correct_num = 0
-    #     for i in tqdm(range(c.test_iterations // num_gpus)):
-    #         ce, correct_num = test_step(next(test_data_iterator))
-    #         # print(loss_all)
-    #         sum_ce += ce 
-    #         sum_correct_num +=  correct_num
+        for epoch_num in range(c.epoch_num):
 
-    #         print('ce: {:.4f}, accuracy: {:.4f}'.format(ce, correct_num / c.batch_size))
-    #     f.write('test: cross entropy loss: {:.4f}, accuracy: {:.4f}\n'.format(sum_ce / c.test_num, sum_correct_num / c.test_num))
+            # train 
+            sum_ce = 0
+            for i in tqdm(range(int(c.train_num // global_batch_size))):
+            # for ds in train_data_iterator:
+                ds = train_data_iterator.get_next()
+                # print(ds)
+                loss_ce = train_step(ds)
+                sum_ce += tf.reduce_sum(loss_ce)
+                
+                # print(loss_ce)
+                # print('ce: {:.4f}'.format(tf.reduce_mean(loss_ce)))
+            print('train: cross entropy loss: {:.4f}\n'.format(sum_ce / c.train_num))
+            f.write('train: cross entropy loss: {:.4f}\n'.format(sum_ce / c.train_num))
 
+            # validate
+            sum_ce = 0
+            sum_correct_num = 0
+            for i in tqdm(range(int(c.test_num // global_batch_size))):
+            # for ds in test_data_iterator:
+                ds = test_data_iterator.get_next()
+                ce, correct_num = test_step(ds)
+                sum_ce += tf.reduce_sum(ce)
+                sum_correct_num +=  tf.reduce_sum(correct_num)
 
-
-
-    for epoch_num in range(c.epoch_num):
-    #     with open(c.log_file, 'a') as f:
-    #         f.write('epoch:{}\n'.format(epoch_num))
-
-            # train
-        sum_ce = 0
-        for i in tqdm(range(int(c.iterations_per_epoch/num_gpus))):
-            loss_ce = train_step(next(iter(train_data_iterator)))
-            # print(loss_all)
-            sum_ce += loss_ce 
-            print('train batch ce loss: {:.4f}'.format(loss_ce))    
-            # f.write('train: cross entropy loss: {:.4f}\n'.format(sum_ce / c.train_num))
-
-
-        sum_ce = 0
-        sum_correct_num = 0
-        for i in tqdm(range(int(c.test_iterations / num_gpus))):
-            ce, correct_num = test_step(next(iter(test_data_iterator)))
-            # print(loss_all)
-            sum_ce += ce 
-            sum_correct_num +=  correct_num
-
-            print('ce: {:.4f}, accuracy: {:.4f}'.format(ce, correct_num / c.batch_size))
-            # f.write('test: cross entropy loss: {:.4f}, accuracy: {:.4f}\n'.format(sum_ce / c.test_num, sum_correct_num / c.test_num))
+            print('test: cross entropy loss: {:.4f}, accuracy: {:.4f}\n'.format(sum_ce / c.test_num, sum_correct_num / c.test_num))
+            f.write('test: cross entropy loss: {:.4f}, accuracy: {:.4f}\n'.format(sum_ce / c.test_num, sum_correct_num / c.test_num))
 
 
-        model.save_weights(c.save_weight_file, save_format='h5')
+            model.save_weights(c.save_weight_file, save_format='h5')
 
-        # save intermediate results
-        if epoch_num % 5 == 4:
-            os.system('cp {} {}_epoch_{}.h5'.format(c.save_weight_file, c.save_weight_file.split('.')[0], epoch_num))
+            # save intermediate results
+            if epoch_num % 5 == 4:
+                os.system('cp {} {}_epoch_{}.h5'.format(c.save_weight_file, c.save_weight_file.split('.')[0], epoch_num))
 
 """In the example above, we iterated over the `dist_dataset` to provide input to your training. We also provide the  `tf.distribute.Strategy.make_experimental_numpy_dataset` to support numpy inputs. You can use this API to create a dataset before calling `tf.distribute.Strategy.experimental_distribute_dataset`.
 Another way of iterating over your data is to explicitly use iterators. You may want to do this when you want to run for a given number of steps as opposed to iterating over the entire dataset.
