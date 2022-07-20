b2d4eb6d60290267bdc037b71f46a43cc63dbdd3
yang.li <yang.li@protagonist-ai.com>
2022-07-20 12:34:04 -0400

    add data argument

--- pretrain/ResNet50/arguments.py
+++ pretrain/ResNet50/arguments.py
@@ -7,13 +7,14 @@ def setup_args():
     设置训练参数
     """
     parser = argparse.ArgumentParser()
+    parser.add_argument('--data', default="", type=str, required=True, help='')
     parser.add_argument('--n_workers', default=1, type=int, required=True, help='')
     parser.add_argument('--category_num', default=1000, type=int, required=False, help='')
     parser.add_argument('--batch_size', default=100, type=int, required=False, help='')
     parser.add_argument('--weight_decay', default= 1e-4, type=float, required=False, help='')
     parser.add_argument('--label_smoothing', default=0.1, type=float, required=False, help='')
     parser.add_argument('--train_num', default=100, type=int, required=False, help='')
-    parser.add_argument("--test_num", help='use distributed training')
+    parser.add_argument("--test_num", default=100, type=int, required=False,help='use distributed training')
     # adv
     parser.add_argument("--initial_learning_rate", default=0.05, type=float)
     parser.add_argument("--minimum_learning_rate", default=0.0001, type=float)
