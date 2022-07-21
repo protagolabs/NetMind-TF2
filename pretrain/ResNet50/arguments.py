113e431677a9f31ee488b53aad8ded6ebc4fa348
yang.li <yang.li@protagonist-ai.com>
2022-07-21 09:44:48 -0400

    add missed argument

--- pretrain/ResNet50/arguments.py
+++ pretrain/ResNet50/arguments.py
@@ -22,5 +22,16 @@ def setup_args():
     parser.add_argument("--save_steps", default=100, type=int)
     parser.add_argument("--num_train_epochs", default=10, type=int)
     parser.add_argument("--warmup_steps", default=10, type=int)
+    
+    parser.add_argument("--do_predict", default=True, type=bool)
+    parser.add_argument("--per_device_eval_batch_size", default=9, type=int)
+    parser.add_argument("--gradient_accumulation_steps", default=1, type=int)
+    parser.add_argument("--adam_beta1", default=10, type=float)
+    parser.add_argument("--adam_epsilon", default=10, type=float)
+    parser.add_argument("--max_grad_norm", default=True, type=float)
+    parser.add_argument("--max_steps", default=9, type=int)
+    parser.add_argument("--warmup_ratio", default=1, type=float)
+    parser.add_argument("--logging_steps", default=10, type=int)
+    parser.add_argument("--fp16", default=False, type=bool)
 
     return parser.parse_args()
