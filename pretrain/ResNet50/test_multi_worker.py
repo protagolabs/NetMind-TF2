33c76ffa9c8f3f5f17bf7f0fab0aa263d095e29f
yang.li <yang.li@protagonist-ai.com>
2022-07-22 10:22:49 -0400

    remove unused log

--- pretrain/ResNet50/test_multi_worker.py
+++ pretrain/ResNet50/test_multi_worker.py
@@ -6,18 +6,23 @@ import tensorflow as tf
 from  keras.backend import set_session
 import config as c
 from tqdm import tqdm
+import  logging
 from utils.data_utils_mm import train_iterator, test_iterator, cnt
 from utils.eval_utils import cross_entropy_batch, correct_num_batch, l2_loss
 from model.ResNet import ResNet
 from NetmindMixins.Netmind import nmp, NetmindDistributedModel, NetmindOptimizer, NetmindDistributedModel
 import sys
 from arguments import setup_args
+logger = logging.getLogger()
+formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(filename)s:%(lineno)d %(message)s', '%Y-%m-%d %H:%M:%S')
+
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.formatter = formatter 
+logger.addHandler(console_handler)
+logger.setLevel(logging.INFO)
 
 args = setup_args()
 
-config = tf.compat.v1.ConfigProto()
-config.gpu_options.allow_growth = True
-session = tf.compat.v1.InteractiveSession(config=config)
 
 os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
 physical_devices = tf.config.list_physical_devices('GPU')
@@ -32,10 +37,10 @@ def allow_memory_growth():
             for gpu in gpus:
                 tf.config.experimental.set_memory_growth(gpu, True)
             logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-            print(f'{len(gpus)} Physical GPUs, {len(logical_gpus)} Logical GPUs')
+            logger.info(f'{len(gpus)} Physical GPUs, {len(logical_gpus)} Logical GPUs')
         except RuntimeError as e:
             # Memory growth must be set before GPUs have been initialized
-            print(e)
+            logger.error(e)
     return
 
 
@@ -114,18 +119,13 @@ def set_input_shape(img, label):
 ## Using `tf.distribute.Strategy` with custom training loops
 
 if __name__ == '__main__':
-    
+
     begin = time.time()
     from tensorflow.python.client import device_lib
-    print(device_lib.list_local_devices())
+    logger.info(device_lib.list_local_devices())
     if not os.getenv('TF_CONFIG'):
         c.tf_config['task']['index'] = int(os.getenv('INDEX'))
         os.environ['TF_CONFIG'] = json.dumps(c.tf_config)
-    print(os.environ['TF_CONFIG'], type(os.environ['TF_CONFIG']))
-    res = json.loads(os.environ['TF_CONFIG'])
-    print(res, type(res))
-    print(res['task']['index'])
-    print('TF_CONFIG : ', os.environ['TF_CONFIG'])
 
     #allow_memory_growth must be called after  tf.distribute.MultiWorkerMirroredStrategy
     multi_worker_mirrored_strategy = tf.distribute.MultiWorkerMirroredStrategy()
@@ -135,7 +135,7 @@ if __name__ == '__main__':
 
     num_gpus = multi_worker_mirrored_strategy.num_replicas_in_sync
 
-    print('Number of devices: {}'.format(num_gpus))
+    logger.info('Number of devices: {}'.format(num_gpus))
     nmp.init(load_checkpoint=False)
 
     n_workers = len(json.loads(os.environ['TF_CONFIG']).get('cluster', {}).get('worker'))
@@ -154,7 +154,7 @@ if __name__ == '__main__':
         # load pretrain
         if c.load_weight_file is not None:
             model.load_weights(c.load_weight_file)
-            # print('pretrain weight l2 loss:{:.4f}'.format(l2_loss(model)))
+            # logger.info('pretrain weight l2 loss:{:.4f}'.format(l2_loss(model)))
         """
 
         iterations_per_epoch = int(args.train_num / args.per_device_train_batch_size)
@@ -167,7 +167,7 @@ if __name__ == '__main__':
                                                         #warm_up_step=int(warm_iterations / n_workers))
 
         optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate_schedules, momentum=0.9)
-        print('scope end')
+        logger.info('scope end')
 
 
 
@@ -177,7 +177,7 @@ if __name__ == '__main__':
         # dataset = train_iterator().batch(global_batch_size)
         # train_data_iterator = multi_worker_mirrored_strategy.experimental_distribute_dataset(dataset)
         # for inputs in train_data_iterator:
-        #    print(train_step(inputs))
+        #    logger.info(train_step(inputs))
         #    # train_step(inputs)
         # train_iterator = train_iterator.map(set_input_shape)
         train_data_path = args.data + "/train"
@@ -195,25 +195,23 @@ if __name__ == '__main__':
 
         t_total = nmp.cur_step
         epochs_trained = nmp.cur_epoch
-        print(f'epochs_trained: {epochs_trained}')
-        print(f'test.py pid : {os.getpid()} ')
+        logger.info(f'epochs_trained: {epochs_trained}')
         next_cnt = 0
         epoch_time_cost_list = []
         with open(c.log_file, 'a') as f:
 
             for epoch_num in range(epochs_trained, args.num_train_epochs):
-                print(f'training with epoch_num : {epoch_num} in range of  {epochs_trained}-------{args.num_train_epochs}')
+                logger.info(f'training with epoch_num : {epoch_num} in range of  {epochs_trained}-------{args.num_train_epochs}')
 
                 # train
                 sum_ce = 0
                 epoch_begin = time.time()
-                print(f'in one epoch : loop start in range {args.train_num // global_batch_size}')
+                logger.info(f'in one epoch : loop start in range {args.train_num // global_batch_size}')
                 for i in tqdm(range(int(args.train_num // global_batch_size))):
                     if nmp.should_skip_step():
                         continue
                     # for ds in train_data_iterator:
                     ds = train_data_iterator.get_next()
-                    print(f'len ds : {len(ds)}, type : {type(ds)}')
                     next_cnt += 1
                     loss_ce = train_step(ds)
                     sum_ce += tf.reduce_sum(loss_ce)
@@ -223,14 +221,14 @@ if __name__ == '__main__':
                     print(type(learing_rate.numpy()), learing_rate.numpy(), f'current  epoch : {epoch_num}')
                     final_loss = float((sum_ce / args.train_num).numpy())
                     final_learing_rate = float(learing_rate.numpy())
-                    print(f"loss : {final_loss}, type: {type(final_loss)}")
-                    print(f"learing_rate : {final_learing_rate}, type: {type(final_learing_rate)}")
+                    logger.info(f"loss : {final_loss}, type: {type(final_loss)}")
+                    logger.info(f"learing_rate : {final_learing_rate}, type: {type(final_learing_rate)}")
                     nmp.step({"loss": final_loss, "Learning rate":final_learing_rate})
-                    print(f'save_pretrained_by_step : {args.save_steps}')
+                    logger.info(f'save_pretrained_by_step : {args.save_steps}')
                     nmp.save_pretrained_by_step(args.save_steps)
 
 
-                print('train: cross entropy loss: {:.4f}\n'.format(sum_ce / args.train_num))
+                logger.info('train: cross entropy loss: {:.4f}\n'.format(sum_ce / args.train_num))
                 f.write('train: cross entropy loss: {:.4f}\n'.format(sum_ce / args.train_num))
 
                 # validate
@@ -242,10 +240,10 @@ if __name__ == '__main__':
                     sum_ce += tf.reduce_sum(ce)
                     sum_correct_num +=  tf.reduce_sum(correct_num)
 
-                print('test: cross entropy loss: {:.4f}, accuracy: {:.4f}\n'.format(sum_ce / args.test_num, sum_correct_num / args.test_num))
+                logger.info('test: cross entropy loss: {:.4f}, accuracy: {:.4f}\n'.format(sum_ce / args.test_num, sum_correct_num / args.test_num))
                 f.write('test: cross entropy loss: {:.4f}, accuracy: {:.4f}\n'.format(sum_ce / args.test_num, sum_correct_num / args.test_num))
                 epoch_end = time.time()
-                epoch_time_cost_list.append(epoch_end - epoch_begin)	
+                epoch_time_cost_list.append(epoch_end - epoch_begin)
 
                 #TODO: netmind already save weights
                 #model.save_weights(c.save_weight_file, save_format='h5')
@@ -255,10 +253,11 @@ if __name__ == '__main__':
                 #    os.system('cp {} {}_epoch_{}.h5'.format(c.save_weight_file, c.save_weight_file.split('.')[0], epoch_num))
             nmp.finish_training()
             end = time.time()
-    
-            print(f'training finished... cnt : {cnt}, next_cnt : {next_cnt}, total cost : {end-begin}, epoch_time_cost_list : {epoch_time_cost_list}')
+
+            logger.info(f'training finished... cnt : {cnt}, next_cnt : {next_cnt}, total cost : {end-begin}, epoch_time_cost_list : {epoch_time_cost_list}')
 
         """In the example above, we iterated over the `dist_dataset` to provide input to your training. We also provide the  `tf.distribute.Strategy.make_experimental_numpy_dataset` to support numpy inputs. You can use this API to create a dataset before calling `tf.distribute.Strategy.experimental_distribute_dataset`.
         Another way of iterating over your data is to explicitly use iterators. You may want to do this when you want to run for a given number of steps as opposed to iterating over the entire dataset.
         The above iteration would now be modified to first create an iterator and then explicity call `next` on it to get the input data.
         """
+
